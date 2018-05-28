190db91a9187d505c18640bd76a6f8dbf07f2724
LuaxY <yann.guineau@voidmx.net>
2018-05-28 16:45:11 +0200

    ServeFile

--- handler/home.go
+++ handler/home.go
@@ -2,21 +2,8 @@ package handler
 
 import (
     "net/http"
-    "text/template"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-    tmpl, err := template.ParseFiles("./public/home.html")
-
-    if err != nil {
-        panic(err)
-    }
-
-    w.Header().Set("Content-Type", "text/html")
-
-    err = tmpl.ExecuteTemplate(w, "home.html", nil)
-
-    if err != nil {
-        panic(err)
-    }
+    http.ServeFile(w, r, "./public/home.html")
 }
