326acf80b856268cdfeaaed33d5d8574dce319f4
LuaxY <yann.guineau@voidmx.net>
2018-07-31 10:48:17 +0200

    Store player info and add rate limit on API

---
+++ handler/game.go
@@ -0,0 +1,94 @@
+package handler
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+)
+
+type Game struct {
+}
+
+type Player struct {
+    Name     string `json:"name"`
+    ServerId uint16 `json:"serverId"`
+    Level    uint16 `json:"level"`
+    Kamas    uint64 `json:"kamas"`
+}
+
+func (h *Game) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(""))
+
+    var player Player
+
+    decoder := json.NewDecoder(r.Body)
+
+    if err := decoder.Decode(&player); err != nil {
+        return
+    }
+
+    if err := player.Store(); err != nil {
+        panic(err)
+    }
+}
+
+func (p *Player) Store() error {
+    file, err := os.OpenFile("public/players.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+
+    if err != nil {
+        return err
+    }
+
+    defer file.Close()
+
+    _, err = file.WriteString(p.String())
+
+    return err
+}
+
+func (p *Player) String() string {
+    var serverName string
+
+    switch p.ServerId {
+    case 36:
+        serverName = "Agride"
+    case 201:
+        serverName = "Echo"
+    case 202:
+        serverName = "Crocabulia"
+    case 203:
+        serverName = "Rubilax"
+    case 204:
+        serverName = "Atcham"
+    case 205:
+        serverName = "Meriana"
+    case 206:
+        serverName = "Pandore"
+    case 207:
+        serverName = "Ush"
+    case 208:
+        serverName = "Julith"
+    case 209:
+        serverName = "Nidas"
+    case 210:
+        serverName = "Merkator"
+    case 211:
+        serverName = "Furye"
+    case 212:
+        serverName = "Brumen"
+    case 222:
+        serverName = "Ilyzaelle"
+    case 50:
+        serverName = "Ombre"
+    case 22:
+        serverName = "Oto Mustam"
+    case 223:
+        serverName = "Temporis I"
+    case 224:
+        serverName = "Temporis II"
+    default:
+        serverName = fmt.Sprintf("Unknown (%d)", p.ServerId)
+    }
+    return fmt.Sprintf("%s|%.30s|%d|%d\r\n", serverName, p.Name, p.Level, p.Kamas)
+}
--- main.go
+++ main.go
@@ -7,17 +7,46 @@ import (
     "SnowBall/handler"
 
     "github.com/gorilla/mux"
+    "github.com/throttled/throttled"
+    "github.com/throttled/throttled/store/memstore"
 )
 
 func main() {
     r := mux.NewRouter()
 
-    r.HandleFunc("/", handler.Home)
+    store, err := memstore.New(65536)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    quota := throttled.RateQuota{
+        MaxRate:  throttled.PerMin(8),
+        MaxBurst: 3,
+    }
+
+    rateLimiter, err := throttled.NewGCRARateLimiter(store, quota)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    httpRateLimiter := throttled.HTTPRateLimiter{
+        RateLimiter: rateLimiter,
+        VaryBy:      &throttled.VaryBy{Path: true},
+    }
+
+    //r.HandleFunc("/", handler.Home)
     r.HandleFunc("/form", handler.Form)
+
+    r.Handle("/game", httpRateLimiter.RateLimit(&handler.Game{})).
+        Methods("POST")
+
+    r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 
     srv := &http.Server{
         Handler: r,
-        Addr:    "127.0.0.1:8000",
+        Addr:    "0.0.0.0:8000",
     }
 
     log.Fatal(srv.ListenAndServe())
